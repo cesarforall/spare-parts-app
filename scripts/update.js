6a019b01b26537053bdeed5b18d0fc2ae8b82cf5
César Almeida <cesaralmeida89@gmail.com>
2023-01-31 17:07:40 +0100

    :hammer: add functions to separate update logic

--- scripts/update.js
+++ scripts/update.js
@@ -1,51 +1,44 @@
 const updateInputElement = document.getElementById('update-input');
-const updateButtonElement = document.getElementById('update-button');
+// const updateButtonElement = document.getElementById('update-button');
 const dataContainerElement = document.getElementById('updated-data');
 
 updateInputElement.addEventListener('change', handleFileAsync, false);
+// updateButtonElement.addEventListener('click', createAndDownloadFiles);
 
-let modelosData;
-let repuestosData;
-let excelFileName;
-let excelLastModifiedDate;
-
-updateButtonElement.addEventListener('click', () => {
-	let newDate = new Date();
-	// lastDate.toString()
-	// console.log(lastDate)
+function createAndDownloadFiles(transformedData, excelLastModifiedDate) {
+	const modelosData = transformedData.models;
+	const repuestosData = transformedData.repuestos;
+	const pageLastModifiedDate = formatDate(new Date());
 
 	const modelosDataString = `const modelosData = ${JSON.stringify(modelosData)};`;
-	const repuestosDataString = `const repuestosData = ${JSON.stringify(repuestosData)};`;
+	const repuestosDataString = `const repuestosData = ${JSON.stringify(repuestosData)}; const excelLastModifiedDate = '${excelLastModifiedDate}'; const pageLastModifiedDate = '${pageLastModifiedDate}'`;
 
+	dataContainerElement.innerText = JSON.stringify(transformedData);
+	
+	alert(`Datos cargados!\nSobreescribe:\ndata/modelos-pax.js\ndata/repuestos-pax.js`);
 	downloadToFile(modelosDataString, 'modelos-pax.js', 'text/plain');
 	downloadToFile(repuestosDataString, 'repuestos-pax.js', 'text/plain');
-});
-
-const downloadToFile = (content, filename, contentType) => {
-	const a = document.createElement('a');
-	const file = new Blob([content], { type: contentType });
-
-	a.href = URL.createObjectURL(file);
-	a.download = filename;
-	a.click();
-
-	URL.revokeObjectURL(a.href);
-};
+}
 
 async function handleFileAsync(e) {
-	/* get first file */
-	excelLastModifiedDate = formatDate(e.target.files[0].lastModifiedDate);
-	excelFileName = e.target.files[0].name;
-	const file = e.target.files[0];
-
-	/* get raw data */
-	const data = await file.arrayBuffer();
-
-	/* data is an ArrayBuffer */
-	const wb = XLSX.read(data, { cellText: false, cellDates: true });
-	console.log(wb);
-
-	transformDataForTheProject(wb);
+	const excelFileName = e.target.files[0].name;
+	
+	if (excelFileName == 'FICHERO REPUESTO PAX_Zelenza Sparesstock all modelsV2_07_11_22.xlsx') {
+		/* get first file */
+		const excelLastModifiedDate = formatDate(e.target.files[0].lastModifiedDate);
+		const file = e.target.files[0];
+		
+		/* get raw data */
+		const data = await file.arrayBuffer();
+		
+		/* data is an ArrayBuffer */
+		const wb = XLSX.read(data, { cellText: false, cellDates: true });
+		const transformedData = transformDataForTheProject(wb);
+		
+		createAndDownloadFiles(transformedData, excelLastModifiedDate);
+	} else {
+		alert('El archivo no es correcto');
+	}
 }
 
 function transformDataForTheProject(wb) {
@@ -69,10 +62,8 @@ function transformDataForTheProject(wb) {
 
 	// get spare parts
 	const spareParts = XLSX.utils.sheet_to_json(theSheet, { header: ['Part number', 'Parts name'], range: 'A4:B463' });
-	// console.log(spareParts);
 
 	const sparePartsLastColums = XLSX.utils.sheet_to_json(theSheet, { header: ['Remark', 'Repair component'], range: 'AF4:AG463', blankrows: true });
-	// console.log(sparePartsLastColums);
 
 	const fullSpareParts = [...spareParts];
 	console.log(fullSpareParts);
@@ -85,11 +76,10 @@ function transformDataForTheProject(wb) {
 		fullSpareParts[index] = { ...fullSpareParts[index], ...item, id: item['__rowNum__'] };
 	}
 
-	// get full componets for a model
+	// get full componets for a single model
 	const modelSpareParts = {};
 
 	const modelComponentList = XLSX.utils.sheet_to_json(theSheet, { header: 'A', range: 'C4:C463' });
-	console.log(modelComponentList);
 
 	function getComponentList(modelId) {
 		const theRange = `${modelId}4:${modelId}463`;
@@ -100,8 +90,6 @@ function transformDataForTheProject(wb) {
 		}
 		return componentList;
 	}
-	const componentC = getComponentList('C');
-	console.log(componentC);
 
 	// Get full list with models and spareparts
 	const fullModelList = [...models];
@@ -111,19 +99,34 @@ function transformDataForTheProject(wb) {
 		model.spareParts = componentList;
 	}
 
-	modelosData = [...fullModelList];
-	repuestosData = [...fullSpareParts];
+	const modelosData = [...fullModelList];
+	const repuestosData = [...fullSpareParts];
 
-	let html = XLSX.utils.sheet_to_html(theSheet, { header: '' });
-	console.log(html);
-	dataContainerElement.innerHTML = html;
+	// let html = XLSX.utils.sheet_to_html(theSheet, { header: '' });
+	// console.log(html);
+	// dataContainerElement.innerHTML = html;
+
+	return { models: modelosData, repuestos: repuestosData };
 }
 
-// Format date
+// Download String object to file
+function downloadToFile(content, filename, contentType) {
+	const a = document.createElement('a');
+	const file = new Blob([content], { type: contentType });
+
+	a.href = URL.createObjectURL(file);
+	a.download = filename;
+	a.click();
+
+	URL.revokeObjectURL(a.href);
+}
+
+// Format Date object to string
 Number.prototype.padLeft = function (base, chr) {
 	var len = String(base || 10).length - String(this).length + 1;
 	return len > 0 ? new Array(len).join(chr || '0') + this : this;
 };
+
 function formatDate(date) {
 	const formatedDate = [date.getDate().padLeft(), (date.getMonth() + 1).padLeft(), date.getFullYear()].join('/') + ' ' + [date.getHours().padLeft(), date.getMinutes().padLeft(), date.getSeconds().padLeft()].join(':');
 	return formatedDate;
